9f7ff86f1faf9623df764d197b9322bcaed582d7
Jungley <yejing@live.cn>
2023-07-16 22:32:54 +0800

    add config.init file

--- README.md
+++ README.md
@@ -4,5 +4,18 @@
 - whitelist # 需要自动代理的
 - local # 不需要代理的
 
+## setup
+
+### 启动docker服务
+```
+docker compose up -d
+```
+
+### 生成订阅链接
+1. 打开浏览器console
+1. 复制`generate.js`内容
+1. 填写订阅地址
+1. 执行后生成clash订阅地址。
+
 ## ref
-- https://gist.github.com/Jungley8/24531669f68bb7ff0eeeefe607737172
+- https://github.com/ACL4SSR/ACL4SSR
---
+++ generate.js
@@ -0,0 +1,27 @@
+// 支持多个订阅地址
+const proxy = [
+    'http://example.com',
+    // 'http://example2.com',
+    // 'http://example3.com',
+]
+
+const params = {
+    target: 'clash',
+    url: proxy.join('|'),
+    insert: false,
+    config: 'https://ghproxy.com/https://github.com/Jungley8/clash-rule/raw/main/config.init',
+    emoji: true,
+    list: false,
+    tfo: false,
+    scv: true,
+    fdn: false,
+    sort: false,
+    new_name: true
+  };
+
+  const queryString = Object.entries(params)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+
+  const url = `http://localhost:25500/sub?${queryString}`;
+  console.log(url);
